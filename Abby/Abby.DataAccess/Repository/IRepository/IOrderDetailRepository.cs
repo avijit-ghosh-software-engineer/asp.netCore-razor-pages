e6609961561cb57096969328bc4be9db4058b0e5
Avijit Ghosh <avijitghsh11@gmail.com>
2023-05-23 00:09:27 +0530

    Section 4 done

---
+++ Abby/Abby.DataAccess/Repository/IRepository/IOrderDetailRepository.cs
@@ -0,0 +1,9 @@
+﻿using Abby.Models;
+
+namespace Abby.DataAccess.Repository.IRepository
+{
+    public interface IOrderDetailRepository : IRepository<OrderDetails>
+    {
+        void Update(OrderDetails obj);
+    }
+}
---
+++ Abby/AbbyWeb/Pages/Admin/FoodTypes/Edit.cshtml.cs
@@ -0,0 +1,36 @@
+using Abby.DataAccess.Repository.IRepository;
+using Abby.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Admin.FoodTypes
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FoodType FoodType { get; set; }
+
+        public EditModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void OnGet(int id)
+        {
+            FoodType = _unitOfWork.FoodType.GetFirstOrDefault(u => u.Id == id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.FoodType.Update(FoodType);
+                _unitOfWork.Save();
+                TempData["success"] = "FoodType updated successfully";
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
