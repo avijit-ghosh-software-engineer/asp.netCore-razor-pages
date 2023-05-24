0e7a6f6e663594832752642a6d0556bd33e86ece
Avijit Ghosh <avijitghsh11@gmail.com>
2023-05-25 01:22:42 +0530

    Section 8 done

--- Abby/AbbyWeb/Program.cs
+++ Abby/AbbyWeb/Program.cs
@@ -12,8 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();//.AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-));
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
